7ecf55442f53f3b0b5c79f689424a2b564ebcedf
root <root@picoctf>
2026-09-16 12:05:59 +0300

    initial

--- src/components/homepage/Milestones.tsx
+++ src/components/homepage/Milestones.tsx
@@ -1,5 +1,5 @@
 import type { User } from "../../types/user";
-import { CircleCheck, CircleDot, Circle, DessertIcon } from "lucide-react";
+import { CircleCheck, CircleDot, Circle } from "lucide-react";
 
 const milestoneList = [
   {
